b6b958aa4fa8d1fccd23ecf5ab5b8301a455d2d8
yujinkimmn <rqwuy8@naver.com>
2021-09-15 16:30:31 +0900

    [chore] 커밋 테스트

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    // 커밋 테스트(0915)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
